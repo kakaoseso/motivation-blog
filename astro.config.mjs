5262bdeafd9477db074d0e5f8e709539122f4bc7
Seongeun So <sungeuns@google.com>
2026-01-28 15:04:59 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://kakaoseso.github.io',
+	base: '/motivation-blog',
 	integrations: [mdx(), sitemap()],
 });
